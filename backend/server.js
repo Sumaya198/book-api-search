230d5ddecb1654057916a2c459be7ade65e307c1
Sumaya Hassan <sumaya_198@hotmail.co.uk>
2021-03-08 00:45:17 +0000

    added enviroment variables

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose")
 const dotenv = require('dotenv')
-const path = require('path')
-const bodyParser = require('body-parser');
-//const booksRouter = require
+
 
 dotenv.config()
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 
 //middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -28,7 +24,10 @@ mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log('successfully connected to the database'))
 .catch(err => console.log('Error' + err))
 
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  }
+  
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
